3c37a648f4208b69950c39fed71388dbb18e225b
guadag12 <guadagonz12@gmail.com>
2025-04-01 17:40:14 -0400

    update goterps

--- R/goterps_presentation.R
+++ R/goterps_presentation.R
@@ -1,26 +1,27 @@
 #' Create and Duplicate Project Structure
 #'
 #' This function creates a new project structure in a specified directory and duplicates all necessary files.
-#' @param path The path where the project structure should be created and files duplicated.
+#' @param dest_dir The path where the project structure should be created and files duplicated.
 #' @export
 
 # Function to recursively copy files and folders
-goterps_presentation <- function(path) {
+goterps_presentation <- function(item, dest_dir) {
 
-  zip_file_path <- paste0(path, ".zip")
 
-  download.file(url = "https://github.com/guadag12/goterps/blob/main/inst/extdata/_extensions/quarto_presentation.zip",
+  zip_file_path <- paste0(dest_dir, ".zip")
+
+
+  download.file(url = "https://github.com/guadag12/goterps/raw/main/inst/extdata/_extensions/quarto_presentation.zip",
                 destfile = zip_file_path)
-  if(!dir.exists(path)) {
-    dir.create(path)
+  if(!dir.exists(dest_dir)) {
+    dir.create(dest_dir)
   } else {
-    unlink(path, recursive = TRUE)
-    dir.create(path)
+    unlink(dest_dir, recursive = TRUE)
+    dir.create(dest_dir)
   }
 
-  unzip(zipfile = zip_file_path, exdir = path)
+  unzip(zipfile = zip_file_path, exdir = dest_dir)
   unlink(zip_file_path)
-
 }
 
 # Example of how to call the function
